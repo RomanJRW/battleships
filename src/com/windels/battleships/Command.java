b0ad205da28ec60c96137dc227472b64391447da
Josh.Windels <joshuawindels@gmail.com>
2016-10-12 22:35:52 +0100

    update Command class with shot location variable

--- src/com/windels/battleships/Command.java
+++ src/com/windels/battleships/Command.java
@@ -4,15 +4,22 @@ class Command {
 	
 	private CommandManager type;
 	private String fileName;
+	private String shotLocation;
 	
 	Command(CommandManager commandType)	{
 		type = commandType;
 		fileName = null;
+		shotLocation = null;
 	}
 	
 	Command(CommandManager commandType, String aFileName)	{
 		this(commandType);
 		fileName = aFileName;
+	}
+	
+	Command(String aShotLocation)	{
+		this(CommandManager.SHOT);
+		shotLocation = aShotLocation;
 	}
 	
 	CommandManager getType()	{
@@ -21,6 +28,10 @@ class Command {
 	
 	String getFileName()	{
 		return fileName;
+	}
+	
+	String getShotLocation()	{
+		return shotLocation;
 	}
 
 }
