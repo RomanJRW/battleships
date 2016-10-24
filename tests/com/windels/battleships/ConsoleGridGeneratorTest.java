6ee2c43c9e83f7d59e5b302ffc68674fb6d8a5d9
Josh.Windels <joshuawindels@gmail.com>
2016-10-25 00:16:16 +0100

    default save file rename and unused dependency removal from grid generator unit test

--- tests/com/windels/battleships/ConsoleGridGeneratorTest.java
+++ tests/com/windels/battleships/ConsoleGridGeneratorTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.Matchers.any;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
